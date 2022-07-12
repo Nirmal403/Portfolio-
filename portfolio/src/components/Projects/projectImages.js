3377c8c872a4cea322732ffc35009f2cdf75989b
Nirmal403 <nirmalcool414@gmail.com>
2022-07-13 00:25:56 +0530

    first commit

---
+++ portfolio/src/components/Projects/projectImages.js
@@ -0,0 +1,16 @@
+import project1Laptop from "../../assets/projects/project-1-laptop.png";
+import project1Mobile from "../../assets/projects/project-1-mobile.png";
+import project2Laptop from "../../assets/projects/project-2-laptop.PNG";
+import project2Mobile from "../../assets/projects/project-2-mobile.PNG";
+import project3Laptop from "../../assets/projects/project-3-laptop.PNG";
+import project3Mobile from "../../assets/projects/project-3-mobile.PNG";
+
+
+export {
+  project1Laptop,
+  project1Mobile,
+  project2Laptop,
+  project2Mobile,
+  project3Laptop,
+  project3Mobile,
+};
---
+++ portfolio/src/components/Projects/projectsData.js
@@ -0,0 +1,36 @@
+import {
+  project1Laptop,
+  project1Mobile,
+  project2Laptop,
+  project2Mobile,
+  project3Laptop,
+  project3Mobile,
+} from "./projectImages";
+
+export const projectData = [
+  {
+    id: 1,
+    image: project1Laptop,
+    transitionImage: project1Mobile,
+    title: "PharmEasy Clone",
+    codeLink: "https://github.com/ribhar/PharmEasy-clone",
+    liveLink: "https://pharmeasyin.netlify.app/",
+  },
+  {
+    id: 2,
+    image: project2Laptop,
+    transitionImage: project2Mobile,
+    title: "UpGrad Clone",
+    codeLink: "https://lnkd.in/g7-yasc7",
+    liveLink: "https://dreamy-snickerdoodle-78bbf8.netlify.app",
+  },
+  {
+    id: 3,
+    image: project3Laptop,
+    transitionImage: project3Mobile,
+    title: "Freshly",
+    codeLink: "https://github.com/Nirmal403/Freshly.git",
+    liveLink: "https://graceful-belekoy-c9fe2f.netlify.app/index.html",
+  },
+  
+];
