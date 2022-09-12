452b15148104a9be1f215050a67ee407f184cec3
Nirmal403 <nirmalcool414@gmail.com>
2022-09-12 21:33:21 +0530

    finalized portfolio

--- portfolio/src/components/About/About.js
+++ portfolio/src/components/About/About.js
@@ -7,11 +7,12 @@ import {
   Resume,
   ResumeLink,
   StyledAbout,
-} from "../styles/About.styled";
+} from "../styles/About.styled";    
 import { SectionHeading, SectionTitle } from "../styles/SectionHeading";
 import { StyledParagraph } from "../styles/Typography.styled";
 import myImage from "../../assets/icons/my-image.jpg";
 import downloadIcon from "../../assets/icons/download-icon.svg";
+
 import resume from "../../assets/Nirmal_Resume.pdf";
 
 const About = () => {
